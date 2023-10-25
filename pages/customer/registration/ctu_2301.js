434d21d027d716aab09f5c51e5eb6f437da9b7e3
cangjie <cangjie@hotmail.com>
2023-10-25 18:26:01 +0800

    form

--- pages/customer/registration/ctu_2301.js
+++ pages/customer/registration/ctu_2301.js
@@ -5,7 +5,73 @@ Page({
    * Page initial data
    */
   data: {
-    status: 0
+    status: 0,
+    submitData:{
+      is_disease: 0,
+      is_allergy: 0
+    }
+  },
+
+  input(e){
+    console.log('input', e)
+    var inputValue = ''
+    var oriValue = e.detail.value
+    var id = e.currentTarget.id
+    var that = this
+    var submitData = that.data.submitData
+    switch(id){
+      case 'child_name':
+        submitData.child_name = oriValue
+        break
+      case 'child_gender':
+        submitData.child_gender = oriValue
+        break
+      case 'child_age':
+        submitData.child_age = oriValue
+        break
+      case 'child_length':
+        submitData.child_length = oriValue
+        break
+      case 'child_weight':
+        submitData.child_weight = oriValue
+        break
+      case 'child_id_no':
+        submitData.child_id_no = oriValue
+        break
+      case 'contact_pri_name':
+        submitData.contact_pri_name = oriValue
+        break
+      case 'contact_pri_cell':
+        submitData.contact_pri_cell = oriValue
+        break
+      case 'contact_sec_name':
+        submitData.contact_sec_name = oriValue
+        break
+      case 'contact_sec_cell':
+        submitData.contact_sec_cell = oriValue
+        break
+      case 'region':
+        for(var i = 0; i < 3; i++){
+          inputValue += oriValue[i] + ((i < 2)?'/':'')
+        }
+        submitData.region = inputValue
+        break;
+      case 'is_disease':
+        submitData.is_disease = oriValue
+        break
+      case 'is_allergy':
+        submitData.is_allergy = oriValue
+        break
+      case 'child_allergy':
+        submitData.child_allergy = oriValue
+        break
+      case 'child_disease':
+        submitData.child_disease = oriValue
+        break
+      default:
+        break
+    }
+    that.setData({submitData: submitData})
   },
 
   /**
